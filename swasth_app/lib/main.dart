8c213162ff150ad22816141f3da445fd816864a5
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-07 23:53:48 +0530

    added app

---
+++ swasth_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:swasth_app/widgets/drawer.dart';
+import 'package:swasth_app/widgets/homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Swasth Bharat'),
+        ),
+        drawer: ShadyDrawer(),
+        body: HomePage(),
+      ),
+    );
+  }
+}
---
+++ swasth_app/lib/widgets/drawer.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ShadyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}
