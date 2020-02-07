cb26ee381c95da089f091ae5b187fc9cf2bc4925
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-08 00:25:07 +0530

    added list tile to the app

--- swasth_app/lib/main.dart
+++ swasth_app/lib/main.dart
@@ -8,12 +8,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Swasth Bharat'),
         ),
-        drawer: ShadyDrawer(),
+        drawer: SizedBox(
+          width: 250,
+          child: ShadyDrawer(),
+        ),
         body: HomePage(),
       ),
     );
--- swasth_app/lib/widgets/drawer.dart
+++ swasth_app/lib/widgets/drawer.dart
@@ -3,6 +3,35 @@ import 'package:flutter/material.dart';
 class ShadyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return Card(
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.healing),
+            title: Text('Doctor'),
+          ),
+          ListTile(
+            leading: Icon(Icons.airline_seat_flat_angled),
+            title: Text('Bed availability'),
+          ),
+          ListTile(
+            leading: Icon(Icons.blur_circular),
+            title: Text('Pharmacy'),
+          ),
+          ListTile(
+            leading: Icon(Icons.accessible),
+            title: Text('Ambulance'),
+          ),
+          ListTile(
+            leading: Icon(Icons.assignment),
+            title: Text('Appointments'),
+          ),
+          ListTile(
+            leading: Icon(Icons.feedback),
+            title: Text('Feedback'),
+          ),
+        ],
+      ),
+    );
   }
 }
