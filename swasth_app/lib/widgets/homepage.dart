2f994b5022d6038b829a065248ed6089ac4a5f30
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-08 01:04:20 +0530

    Added carousel on homepage

--- swasth_app/lib/widgets/homepage.dart
+++ swasth_app/lib/widgets/homepage.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:carousel_pro/carousel_pro.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Content will be added soon'),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            "Goverment Health Insurance Schemes",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 150.0,
+          width: 300.0,
+          child: Carousel(
+            images: [
+              Image.asset('assets/1.jpg'),
+              Image.asset('assets/2.jpg'),
+              Image.asset('assets/3.jpg'),
+              Image.asset('assets/4.jpg'),
+              Image.asset('assets/5.jpg'),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
