608a2d5688a7e93c4d89065a58b2bcff0ccb23f5
saumyabhatnagar1 <bhatnagar.saumya2000@gmail.com>
2020-02-08 02:26:15 +0530

    chatbot added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -25,15 +25,64 @@ mongoose.connect('mongodb://127.0.0.1:27017/sih-main',{
 //     console.log(e);
 // })
 
-// const Image =mongoose.model('Image',{
-//     image:{
-//         type:Buffer,
-//         required:true
-//     },
-//     filename:{
-//         type:String
-//     }
+// const one =new Doctor({
+//     name:'Dr Rahul Arora',
+//     available:'yes'
 // })
+// const two= new Doctor({
+//     name:'Dr. Santosh Tyagi',
+//     available:'yes'
+// })
+// const three=new Doctor({
+//     name:'Dr. Anurag Triphati',
+//     available:'yes'
+// })
+// const four=new Doctor({
+//     name:'Dr. Hardik Sharma',
+//     available:'yes'
+// })
+// const five =new Doctor({
+//     name:'Dr. Feroz Shah',
+//     available:'yes'
+// })
+// const six=new Doctor({
+//     name:'Dr. Vivek Goyal',
+//     available:'yes'
+// })
+
+
+// one.save().then(()=>{
+//     console.log('created')
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+// two.save().then(()=>{
+//     console.log('created')
+// }).catch((e)=>{
+//     console.log(e)
+// })
+// three.save().then(()=>{
+//     console.log('created')
+// }).catch((e)=>{
+//     console.log(e)
+// })
+// four.save().then(()=>{
+//     console.log('created')
+// }).catch((e)=>{
+//     console.log(e)
+// })
+// five.save().then(()=>{
+//     console.log('created')
+// }).catch((e)=>{
+//     console.log(e)
+// })
+// six.save().then(()=>{
+//     console.log('created')
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
 
  
 
