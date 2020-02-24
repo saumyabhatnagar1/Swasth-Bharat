bc8ae042381af5f9d45e7d329de6e847e2ccc905
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-24 13:35:41 +0530

    Adding gif of app

--- swasth_app/lib/widgets/doctor.dart
+++ swasth_app/lib/widgets/doctor.dart
@@ -7,70 +7,72 @@ class ShadyDoctor extends StatelessWidget {
       appBar: AppBar(
         title: Text('Swasth Bharat'),
       ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 14.0,
-          ),
-          Card(
-            child: TextField(
-              textAlign: TextAlign.center,
-              decoration: new InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Search Doctors',
-                hintStyle: TextStyle(
-                  fontSize: 18.0,
-                ),
-                icon: Icon(
-                  Icons.search,
-                  size: 40.0,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 14.0,
+            ),
+            Card(
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: new InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search Doctors',
+                  hintStyle: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                  icon: Icon(
+                    Icons.search,
+                    size: 40.0,
+                  ),
                 ),
               ),
             ),
-          ),
-          Card(
-            child: Container(
-              height: 700,
-              child: ListView(
-                children: ListTile.divideTiles(
-                  context: context,
-                  tiles: [
-                    ListTile(
-                      subtitle: Text('Neurologist specialist'),
-                      title: Text('Dr. Rahul Arora'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                    ),
-                    ListTile(
-                      subtitle: Text('Neurologist specialist'),
-                      title: Text('Dr. Santosh Tyagi'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                    ),
-                    ListTile(
-                      subtitle: Text('Neurologist specialist'),
-                      title: Text('Dr. Anurag Tripathi'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                    ),
-                    ListTile(
-                      subtitle: Text('Neurologist specialist'),
-                      title: Text('Dr. Hardik Sharma'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                    ),
-                    ListTile(
-                      subtitle: Text('Neurologist specialist'),
-                      title: Text('Dr. Feroz Shah'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                    ),
-                    ListTile(
-                      subtitle: Text('Neurologist specialist'),
-                      title: Text('Dr. Vivek Goyal'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                    ),
-                  ],
-                ).toList(),
+            Card(
+              child: Container(
+                height: 700,
+                child: ListView(
+                  children: ListTile.divideTiles(
+                    context: context,
+                    tiles: [
+                      ListTile(
+                        subtitle: Text('Neurologist specialist'),
+                        title: Text('Dr. Rahul Arora'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        subtitle: Text('Neurologist specialist'),
+                        title: Text('Dr. Santosh Tyagi'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        subtitle: Text('Neurologist specialist'),
+                        title: Text('Dr. Anurag Tripathi'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        subtitle: Text('Neurologist specialist'),
+                        title: Text('Dr. Hardik Sharma'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        subtitle: Text('Neurologist specialist'),
+                        title: Text('Dr. Feroz Shah'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        subtitle: Text('Neurologist specialist'),
+                        title: Text('Dr. Vivek Goyal'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ],
+                  ).toList(),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
